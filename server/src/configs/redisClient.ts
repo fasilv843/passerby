98c12d2d5a4c440d95b543efac65943a28a8443b
Fasil <fasilvaliyattil843@gmail.com>
2025-09-17 19:18:49 +0530

    ref: handled redis events

--- server/src/configs/redisClient.ts
+++ server/src/configs/redisClient.ts
@@ -2,3 +2,24 @@ import Redis from "ioredis";
 import { envConfig } from "./environments";
 
 export const redis = new Redis(envConfig.redisUrl);
+
+
+redis.on("connect", () => {
+  console.log("✅ Connected to Redis");
+});
+
+redis.on("ready", () => {
+  console.log("Redis is ready to use");
+});
+
+redis.on("error", (err) => {
+  console.error("❌ Redis error:", err);
+});
+
+redis.on("close", () => {
+  console.warn("⚠ Redis connection closed");
+});
+
+redis.on("reconnecting", (time: number) => {
+  console.log(`🔄 Redis reconnecting in ${time}ms`);
+});
